a31e5d8dfe6f315a675bf1c42966a8a7472fe468
Marlon <marmoneymar@icloud.com>
2019-11-11 20:29:15 -0500

    Setup carthage & swiftlint

--- swaap/swaapTests/swaapTests.swift
+++ swaap/swaapTests/swaapTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import swaap
 
-class swaapTests: XCTestCase {
+class SwaapTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
