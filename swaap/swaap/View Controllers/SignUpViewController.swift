b04899e7c460494d4e279e4b7c0e341dfbc8206c
Marlon <marmoneymar@icloud.com>
2019-11-20 18:44:56 -0500

    Implemented basic signup logic to verify password match, strength, & ...
    verifying email format

--- swaap/swaap/View Controllers/SignUpViewController.swift
+++ swaap/swaap/View Controllers/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Auth0
 
 class SignUpViewController: UIViewController {
 	@IBOutlet private weak var usernameForm: FormInputView!
@@ -28,5 +29,42 @@ class SignUpViewController: UIViewController {
 		emailForm.keyboardType = .emailAddress
 		passwordForm.contentType = .newPassword
 		passwordConfirmForm.contentType = .newPassword
+//		signUpButton.isEnabled = false
+	}
+
+	@IBAction func signupTapped(_ sender: ButtonHelper) {
+		guard let (email, password) = checkFormValidity() else { return }
+		Auth0.authentication()
+			.createUser(email: email,
+						password: password,
+						connection: "Username-Password-Authentication",
+						userMetadata: ["first_name": "First", "last_name": "Last"])
+			.start { result in
+				switch result {
+				case .success(let user):
+					print("User Signed up: \(user)")
+				case .failure(let error):
+					print("Failed with \(error)")
+				}
+		}
+	}
+
+	private func checkEmail() -> String? {
+		(emailForm.text?.isEmail ?? false) ? emailForm.text : nil
+	}
+
+	private func checkPasswordStrength() -> String? {
+		guard let password = passwordForm.text, password.count >= 8 else { return nil }
+		return password
+	}
+
+	private func checkPasswordMatch() -> Bool {
+		passwordForm.text == passwordConfirmForm.text
+	}
+
+	private func checkFormValidity() -> (email: String, password: String)? {
+		guard let email = checkEmail(), let password = checkPasswordStrength(), checkPasswordMatch() else { return nil }
+		signUpButton.isEnabled = true
+		return (email, password)
 	}
 }
