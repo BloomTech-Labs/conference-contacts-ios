2e1eaafc90cc5d442f9f2b8b1efaf4e1376f9b43
Michael Redig <mredig@gmail.com>
2019-11-29 03:03:07 -0600

    added range normalizing extensions
    added range normalizing tests

--- swaap/swaapTests/swaapTests.swift
+++ swaap/swaapTests/swaapTests.swift
@@ -18,4 +18,26 @@ class SwaapTests: XCTestCase {
 //	override func tearDown() {
 //		// Put teardown code here. This method is called after the invocation of each test method in the class.
 //	}
+
+	func testRangeNormalizing() {
+		let rangeA = 0...10
+		let rangeB = -10...10
+		let rangeC = -5.0...11.5
+
+		XCTAssertEqual(0, rangeA.normalizedIndex(0))
+		XCTAssertEqual(1, rangeA.normalizedIndex(10))
+		XCTAssertEqual(0.2, rangeA.normalizedIndex(2))
+		XCTAssertEqual(-1, rangeA.normalizedIndex(-10))
+
+		XCTAssertEqual(0, rangeB.normalizedIndex(-10))
+		XCTAssertEqual(1, rangeB.normalizedIndex(10))
+		XCTAssertEqual(0.5, rangeB.normalizedIndex(0))
+		XCTAssertEqual(1.5, rangeB.normalizedIndex(20))
+
+		XCTAssertEqual(0, rangeC.normalizedIndex(-5))
+		XCTAssertEqual(1, rangeC.normalizedIndex(11.5))
+		XCTAssertEqual(0.5, rangeC.normalizedIndex(3.25))
+		XCTAssertEqual(1.5, rangeC.normalizedIndex(19.75))
+
+	}
 }
