242d4a839ec31a6b76858ff7311e1c2055dc5fc3
Marlon <marmoneymar@icloud.com>
2019-12-05 04:14:56 -0500

    Started working on Edit profile view controller

--- swaap/swaap/View Controllers/EditProfileViewController.swift
+++ swaap/swaap/View Controllers/EditProfileViewController.swift
@@ -18,11 +18,19 @@ class EditProfileViewController: UIViewController {
 	@IBOutlet private weak var profileImageView: UIImageView!
 	@IBOutlet private weak var choosePhotoButton: UIButton!
 
-
 	override func viewDidLoad() {
         super.viewDidLoad()
-		navigationController?.setNavigationBarHidden(true, animated: true)
+		navigationController?.setNavigationBarHidden(false, animated: false)
+		navigationController?.navigationBar.installBlurEffect()
+		setupUI()
     }
+
+	private func setupUI() {
+		profileImageView.layer.borderColor = UIColor.systemGray5.cgColor
+		profileImageView.layer.borderWidth = 1.5
+		profileImageView.layer.cornerRadius = 20
+		profileImageView.layer.cornerCurve = .continuous
+	}
     
 	@IBAction func saveButtonTapped(_ sender: UIButton) {
 	}
@@ -35,4 +43,14 @@ class EditProfileViewController: UIViewController {
 		imageActionSheet()
 	}
 
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		picker.dismiss(animated: true)
+	}
+
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		picker.dismiss(animated: true)
+		guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+		profileImageView.image = image
+		choosePhotoButton.setImage(nil, for: .normal)
+	}
 }
