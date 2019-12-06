5484d4354671605d2e45a1c2f2fe6c88c90d0348
Michael Redig <mredig@gmail.com>
2019-12-06 02:44:18 -0600

    set up profile model object instance
    c in crud for profile modification
    cache profile locally

--- swaap/swaap/Model Controllers/ProfileController.swift
+++ swaap/swaap/Model Controllers/ProfileController.swift
@@ -16,7 +16,15 @@ protocol ProfileAccessor: AnyObject {
 class ProfileController {
 	let authManager: AuthManager
 
-	var userProfile: UserProfile?
+	private var _userProfile: UserProfile?
+	/// Automatically sends `userProfileChanged`, `userProfilePopulated`, or `userProfileDepopulated` notifications when modified
+	var userProfile: UserProfile? {
+		get { _userProfile }
+		set {
+			checkUserProfileChanged(oldValue: _userProfile, newValue: newValue)
+			_userProfile = newValue
+		}
+	}
 
 	let baseURL = URL(string: "https://lambda-labs-swaap-staging.herokuapp.com/")!
 	var graphqlURL: URL {
@@ -24,19 +32,18 @@ class ProfileController {
 	}
 	let networkHandler = NetworkHandler()
 
+	// MARK: - Lifecycle
 	init(authManager: AuthManager) {
 		self.authManager = authManager
 	}
 
+	// MARK: - Networking
 	func createProfileOnServer(completion: @escaping (Bool) -> Void) {
-		guard let idClaims = authManager.idClaims, let accessToken = authManager.credentials?.accessToken else {
+		guard let (idClaims, cRequest) = networkCommon() else {
 			completion(false)
 			return
 		}
-		var request = graphqlURL.request
-		request.addValue("application/json", forHTTPHeaderField: HTTPHeaderKeys.contentType.rawValue)
-		request.addValue(accessToken, forHTTPHeaderField: HTTPHeaderKeys.auth.rawValue)
-		request.httpMethod = HTTPMethods.post.rawValue
+		var request = cRequest
 
 		let mutation = "mutation CreateUser($user: CreateUserInput!) { createUser(data: $user) { success code message } }"
 		let userInfo = ["user": CreateUser(sub: idClaims.sub, name: idClaims.name, picture: idClaims.picture, email: idClaims.email)]
@@ -51,6 +58,7 @@ class ProfileController {
 			return
 		}
 
+		request.expectedResponseCodes = [201]
 		networkHandler.transferMahCodableDatas(with: request) { (result: Result<[String: [String: UserMutationResponse]], NetworkError>) in
 			do {
 				let responseDict = try result.get()
@@ -68,4 +76,81 @@ class ProfileController {
 			}
 		}
 	}
+
+	private func networkCommon() -> (Auth0IDClaims, NetworkRequest)? {
+		guard let idClaims = authManager.idClaims, let accessToken = authManager.credentials?.accessToken else { return nil }
+		var request = graphqlURL.request
+		request.addValue(.contentType(type: .json), forHTTPHeaderField: .commonKey(key: .contentType))
+		request.addValue(.other(value: accessToken), forHTTPHeaderField: .commonKey(key: .authorization))
+		request.httpMethod = .post
+		return (idClaims, request)
+	}
+
+	// MARK: - Local Storage
+	private let fm = FileManager.default
+	private let profileCacheFilename = "profile.plist"
+	private var profileCacheURL: URL {
+		guard let cacheFolder = fm.urls(for: .cachesDirectory, in: .userDomainMask).first else { preconditionFailure("Something catastophically wrong occurred...") }
+		return cacheFolder.appendingPathComponent(profileCacheFilename)
+	}
+
+	private func loadCachedProfile() {
+		guard fm.fileExists(atPath: profileCacheURL.path), let claims = authManager.idClaims else { return }
+		let data: Data
+		do {
+			data = try Data(contentsOf: profileCacheURL)
+		} catch {
+			NSLog("Error loading cached profile data: \(error)")
+			return
+		}
+
+		do {
+			let userProfile = try PropertyListDecoder().decode(UserProfile.self, from: data)
+			if userProfile.authID == claims.authID {
+				self.userProfile = userProfile
+			}
+		} catch {
+			NSLog("Error decoding cached profile data: \(error)")
+		}
+	}
+
+	private func saveProfileToCache() {
+		guard let profile = userProfile else { return }
+
+		let data: Data
+		do {
+			data = try PropertyListEncoder().encode(profile)
+		} catch {
+			NSLog("Error encoding profile cache: \(error)")
+			return
+		}
+
+		do {
+			try data.write(to: profileCacheURL)
+		} catch {
+			NSLog("Error saving profile cache: \(error)")
+		}
+	}
+}
+
+// MARK: - Notifications
+extension ProfileController {
+	private func checkUserProfileChanged(oldValue: UserProfile?, newValue: UserProfile?) {
+		guard oldValue != newValue else { return }
+		let nc = NotificationCenter.default
+		if oldValue == nil {
+			nc.post(name: .userProfilePopulated, object: nil)
+		} else if newValue == nil {
+			nc.post(name: .userProfileDepopulated, object: nil)
+		} else {
+			nc.post(name: .userProfileChanged, object: nil)
+		}
+		saveProfileToCache()
+	}
+}
+
+extension NSNotification.Name {
+	static let userProfileChanged = NSNotification.Name("com.swaap.userProfileChanged")
+	static let userProfilePopulated = NSNotification.Name("com.swaap.userProfilePopulated")
+	static let userProfileDepopulated = NSNotification.Name("com.swaap.userProfileDepopulated")
 }
