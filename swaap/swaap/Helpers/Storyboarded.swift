6a3d70b02ea29c1c8a4c7f6b37fea798ff7affbd
Marlon <marmoneymar@icloud.com>
2019-12-09 20:12:35 -0500

    Added new storyboarded instantiation

--- swaap/swaap/Helpers/Storyboarded.swift
+++ swaap/swaap/Helpers/Storyboarded.swift
@@ -27,4 +27,20 @@ extension Storyboarded where Self: UIViewController {
 			else { fatalError("Storyboard \(name) has no view controller identified as \(className)") }
 		return storyboardedVC
     }
+
+	static func instantiate(storyboardName name: String = "Main", with customInitializer: @escaping (NSCoder) -> UIViewController?) -> Self {
+        // this pulls out "MyApp.MyViewController"
+        let fullName = NSStringFromClass(self)
+
+        // this splits by the dot and uses everything after, giving "MyViewController"
+        let className = fullName.components(separatedBy: ".")[1]
+
+        // load our storyboard
+        let storyboard = UIStoryboard(name: name, bundle: Bundle.main)
+
+        // instantiate a view controller with that identifier, and force cast as the type that was requested
+		guard let storyboardedVC = storyboard.instantiateViewController(identifier: className, creator: customInitializer) as? Self
+			else { fatalError("Storyboard \(name) has no view controller identified as \(className)") }
+		return storyboardedVC
+    }
 }
