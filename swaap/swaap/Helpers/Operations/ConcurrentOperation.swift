3db6c51c86040387a5d412ed400b7c219a93c30e
Michael Redig <mredig@gmail.com>
2020-01-04 23:18:21 -0700

    spaces to tabs in a file

--- swaap/swaap/Helpers/Operations/ConcurrentOperation.swift
+++ swaap/swaap/Helpers/Operations/ConcurrentOperation.swift
@@ -9,64 +9,64 @@
 import Foundation
 
 class ConcurrentOperation: Operation {
-    
-    // MARK: Types
-    
-    enum State: String {
-        case isReady, isExecuting, isFinished
-    }
-    
-    // MARK: Properties
+	
+	// MARK: Types
+	
+	enum State: String {
+		case isReady, isExecuting, isFinished
+	}
+	
+	// MARK: Properties
 	let task: () -> Void
-    
-    private var _state = State.isReady
-    
-    private let stateQueue = DispatchQueue(label: "com.swaap.concurrentStateQueue")
-    var state: State {
-        get {
-            var result: State?
-            let queue = self.stateQueue
-            queue.sync {
-                result = _state
-            }
-            return result!
-        }
-        
-        set {
-            let oldValue = state
-            willChangeValue(forKey: newValue.rawValue)
-            willChangeValue(forKey: oldValue.rawValue)
-            
-            stateQueue.sync { self._state = newValue }
-            
-            didChangeValue(forKey: oldValue.rawValue)
-            didChangeValue(forKey: newValue.rawValue)
-        }
-    }
-    
-    // MARK: NSOperation
-    
-    override dynamic var isReady: Bool {
-        return super.isReady && state == .isReady
-    }
-    
-    override dynamic var isExecuting: Bool {
-        return state == .isExecuting
-    }
-    
-    override dynamic var isFinished: Bool {
-        return state == .isFinished
-    }
-    
-    override var isAsynchronous: Bool {
-        return true
-    }
-
+	
+	private var _state = State.isReady
+	
+	private let stateQueue = DispatchQueue(label: "com.swaap.concurrentStateQueue")
+	var state: State {
+		get {
+			var result: State?
+			let queue = self.stateQueue
+			queue.sync {
+				result = _state
+			}
+			return result!
+		}
+		
+		set {
+			let oldValue = state
+			willChangeValue(forKey: newValue.rawValue)
+			willChangeValue(forKey: oldValue.rawValue)
+			
+			stateQueue.sync { self._state = newValue }
+			
+			didChangeValue(forKey: oldValue.rawValue)
+			didChangeValue(forKey: newValue.rawValue)
+		}
+	}
+	
+	// MARK: NSOperation
+	
+	override dynamic var isReady: Bool {
+		return super.isReady && state == .isReady
+	}
+	
+	override dynamic var isExecuting: Bool {
+		return state == .isExecuting
+	}
+	
+	override dynamic var isFinished: Bool {
+		return state == .isFinished
+	}
+	
+	override var isAsynchronous: Bool {
+		return true
+	}
+	
 	init(task: @escaping () -> Void) {
 		self.task = task
 		super.init()
 	}
-
+	
 	override func start() {
 		defer {
 			state = .isFinished
