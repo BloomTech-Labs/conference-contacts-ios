2038211ec850c8cfc018677544ab4fe8f12a65d3
Chad Rutherford <chad_rutherford@outlook.com>
2020-03-26 18:16:39 -0400

    Removed QRCode from separate ViewController and moved it to the ConnectViewController

--- swaap/swaap/Support/AppDelegate.swift
+++ swaap/swaap/Support/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Auth0
-import TouchVisualizer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,11 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// only show touches when debugging for screensharing and recording - dont show touches when on testflight or app store
-		if ReleaseState.current == .debug {
-			var config = Configuration()
-			config.color = UIColor.lightGray.withAlphaComponent(0.25)
-			Visualizer.start(config)
-		}
+//		if ReleaseState.current == .debug {
+//			var config = Configuration()
+//			config.color = UIColor.lightGray.withAlphaComponent(0.25)
+//			Visualizer.start(config)
+//		}
 		return true
 	}
 
