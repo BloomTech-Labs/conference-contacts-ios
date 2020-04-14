fa9d5b6680382b898d79fc3fe782ffa54718652a
Chad Rutherford <chad_rutherford@outlook.com>
2020-04-14 18:33:45 -0400

    Fixed Onboarding to be more cohesive

---
+++ swaap/swaap/View Controllers/Onboarding/ScreenFive.swift
@@ -0,0 +1,13 @@
+//
+//  ScreenFive.swift
+//  swaap
+//
+//  Created by Chad Rutherford on 4/14/20.
+//  Copyright © 2020 swaap. All rights reserved.
+//
+
+import UIKit
+
+class ScreenFive: UIViewController {
+	
+}
