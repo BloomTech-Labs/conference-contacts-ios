86bded001792100422b4f51bc71ef88f5561ab9e
Chad Rutherford <chad_rutherford@outlook.com>
2020-04-29 19:50:39 -0400

    Last fixes. Removing errors and warnings. Preparing project for next team.

--- swaap/swaap/Models/Contact Method Model/ProfileFieldPrivacy.swift
+++ swaap/swaap/Models/Contact Method Model/ProfileFieldPrivacy.swift
@@ -8,24 +8,24 @@
 
 import Foundation
 
-/// As the app is, there's not really much point to `public` right now. 
+/// As the app is, there's not really much point to `public` right now.
 enum ProfileFieldPrivacy: String, Codable, Hashable {
 	init(from decoder: Decoder) throws {
 		let container = try decoder.singleValueContainer()
 		let rawValue = try container.decode(String.self)
 		let lcValue = rawValue.lowercased()
-
+		
 		guard let decoded = ProfileFieldPrivacy(rawValue: lcValue) else {
 			throw ProfileDecodingError.unknownValue(value: rawValue)
 		}
 		self = decoded
 	}
-
+	
 	func encode(to encoder: Encoder) throws {
 		var container = encoder.singleValueContainer()
 		try container.encode(rawValue.uppercased())
 	}
-
+	
 	case `public`
 	case `private`
 }
