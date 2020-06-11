aa41a34d164bf92c6a0b41bf3be04c6aa23c64ed
Keri Levesque <kerianne166@gmail.com>
2020-06-11 13:01:49 -0400

    removed apple sign in from login view controller

--- swaap/swaap/View Controllers/LogInViewController.swift
+++ swaap/swaap/View Controllers/LogInViewController.swift
@@ -17,7 +17,6 @@ class LogInViewController: UIViewController {
 	// MARK: - Outlets
 	@IBOutlet private weak var signupButton: UIButton!
 	@IBOutlet private weak var mainStackView: UIStackView!
-	@IBOutlet private weak var appleSigninContainer: UIView!
 	@IBOutlet private weak var googleSigninButton: ButtonHelper!
 
 	let appleAuthButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
@@ -40,7 +39,6 @@ class LogInViewController: UIViewController {
 
 	override func viewDidLoad() {
         super.viewDidLoad()
-		configureAppleAuthButton()
 		setupUI()
     }
 
@@ -48,31 +46,7 @@ class LogInViewController: UIViewController {
 		overrideUserInterfaceStyle = .light
 	}
 
-	private func configureAppleAuthButton() {
-		appleSigninContainer.addSubview(appleAuthButton)
-		appleSigninContainer.backgroundColor = .clear
-		appleAuthButton.translatesAutoresizingMaskIntoConstraints = false
-
-		appleAuthButton.topAnchor.constraint(equalTo: appleSigninContainer.topAnchor).isActive = true
-		appleAuthButton.bottomAnchor.constraint(equalTo: appleSigninContainer.bottomAnchor).isActive = true
-		appleAuthButton.leadingAnchor.constraint(equalTo: appleSigninContainer.leadingAnchor).isActive = true
-		appleAuthButton.trailingAnchor.constraint(equalTo: appleSigninContainer.trailingAnchor).isActive = true
-
-		[appleAuthButton].forEach { $0.addTarget(self, action: #selector(handleSignInWithAppleIDButtonTap), for: .touchUpInside) }
-	}
-
-	// MARK: - IBActions
-	@objc private func handleSignInWithAppleIDButtonTap(_ sender: ASAuthorizationAppleIDButton?) {
-		authManager.signInWithApple(completion: { [weak self] error in
-			guard let self = self else { return }
-			if let error = error {
-				self.showErrorAlert(error)
-				return
-			}
-			self.initiateProfileFetch()
-		})
-	}
-
+    //MARK: Actions
 	@IBAction func googleSignInTapped(_ sender: ButtonHelper) {
 		authManager.showWebAuth(completion: { [weak self] error in
 			guard let self = self else { return }
